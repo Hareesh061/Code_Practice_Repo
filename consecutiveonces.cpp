eb7d11ef1c8b0a2e38a89c019936c9cb631fe6d8
Hareesh Naik <hareeshnaik836@gmail.com>
2024-03-13 19:54:00 +0530

    added

---
+++ consecutiveonces.cpp
@@ -0,0 +1,17 @@
+//max consecutive once in arr
+
+
+int findMaxConsecutiveOnes(vector < int > & nums) {
+      int cnt = 0;
+      int maxi = 0;
+      for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == 1) {
+          cnt++;
+        } else {
+          cnt = 0;
+        }
+
+        maxi = max(maxi, cnt);
+      }
+      return maxi;
+    }
